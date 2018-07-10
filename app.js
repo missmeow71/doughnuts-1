f73a843eaad0a56457aab745d4c43ec6185a421a
missmeow71 <sensuousdesigner@gmail.com>
2018-07-10 11:11:40 -0600

    inmitial commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express()
 const port = process.env.PORT || 3000
 const fixtures = require('./data/fixtures')
 
+const app = express()
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
